79eb5db201ac5b2dea98eb87beefa93f91fae8c2
shmykov.alexander <shmykov.alexander@gmail.com>
2025-03-30 17:10:21 +0300

    Перед изменением FW

---
+++ PostgreEFWebApi/Data/WebApiDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace PostgreEFWebApi.Data
+{
+    public class WebApiDbContext:DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public WebApiDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("EFDemo"));
+        }
+        public DbSet<PostgreEFWebApi.Entities.Person> Persons { get; set; }
+        public DbSet<PostgreEFWebApi.Entities.Address> Addresses { get; set; }
+
+    }
+}
--- PostgreEFWebApi/Entities/Person.cs
+++ PostgreEFWebApi/Entities/Person.cs
@@ -2,5 +2,10 @@
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string FistName { get; set; } = "Dj Dow";
+        public  string? SecondName { get; set; }
+        public string? LastName { get; set; } 
+        public List<Address> Addresses { get; set; }
     }
 }
